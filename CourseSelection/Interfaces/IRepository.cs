c6efd03636bfc29f4f189a6aa9ce728637c1a007
ElaineKang <elaine5030331@gmail.com>
2024-10-15 21:30:35 +0800

    feat: add `IRepository` Interface and implement(`Services.EFRepository`)

---
+++ CourseSelection/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace CourseSelection.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> UpdateRangeAsync(IEnumerable<TEntity> entities);
+        Task DeleteAsync(TEntity entity);
+        Task DeleteRangeAsync(IEnumerable<TEntity> entities);
+        Task<TEntity> GetByIdAsync<TId>(TId id);
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> expression);
+        Task<List<TEntity>> ListAsync(Expression<Func<TEntity, bool>> expression);
+    }
+}
---
+++ CourseSelection/Services/EFRepository.cs
@@ -0,0 +1,72 @@
+﻿using CourseSelection.Data;
+using CourseSelection.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CourseSelection.Services
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected CourseSelectionContext DbContext;
+        protected DbSet<TEntity> DbSet;
+
+        public EFRepository(CourseSelectionContext dbContext, DbSet<TEntity> dbSet)
+        {
+            DbContext = dbContext;
+            DbSet = dbContext.Set<TEntity>();
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await DbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await DbSet.AnyAsync(expression);
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            DbSet.Remove(entity);
+            await DbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteRangeAsync(IEnumerable<TEntity> entities)
+        {
+            DbSet.RemoveRange(entities);
+            await DbContext.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return (await DbSet.FirstOrDefaultAsync(expression))!;
+        }
+
+        public async Task<TEntity> GetByIdAsync<TId>(TId id)
+        {
+            return (await DbSet.FindAsync(new object[] { id }))!;
+        }
+
+        public async Task<List<TEntity>> ListAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await DbSet.Where(expression).ToListAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await DbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> UpdateRangeAsync(IEnumerable<TEntity> entities)
+        {
+            DbSet.UpdateRange(entities);
+            await DbContext.SaveChangesAsync();
+            return entities;
+        }
+    }
+}
